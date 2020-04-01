1351ec793c0d2911c56995601f3b975b2416ded2
SebG-prog <sebastien-gruet@orange.fr>
2020-04-01 12:24:58 +0200

    updated project with ContactList component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from "react";
-import Contact from "./components/Contact";
+// import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        name="Kaylee Shaw"
-        avatar="https://randomuser.me/api/portraits/women/65.jpg"
-        online={true}
-      />
-      <Contact
-        name="Alex Wheeler"
-        avatar="https://randomuser.me/api/portraits/men/94.jpg"
-        online={false}
-      />
-      <Contact
-        name="Salvador Porter"
-        avatar="https://randomuser.me/api/portraits/men/21.jpg"
-        online={true}
-      />
+      <ContactList/>
     </div>
   );
 }
