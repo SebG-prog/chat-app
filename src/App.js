15c841c7b7c9837af4864da612a6871015386cff
SebG-prog <sebastien-gruet@orange.fr>
2020-04-01 11:49:42 +0200

    updated local version with the sandbox online version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import Contact from "./components/Contact"
+import React from "react";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-      <Contact/>
+      <Contact
+        name="Kaylee Shaw"
+        avatar="https://randomuser.me/api/portraits/women/65.jpg"
+        online={true}
+      />
+      <Contact
+        name="Alex Wheeler"
+        avatar="https://randomuser.me/api/portraits/men/94.jpg"
+        online={false}
+      />
+      <Contact
+        name="Salvador Porter"
+        avatar="https://randomuser.me/api/portraits/men/21.jpg"
+        online={true}
+      />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import { } from "module";
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
+import PropTypes from "prop-types";
 
-const isOnline = true
-
-function Contact() {
-    return (
-        <div className="Contact">
-            <img className="avatar" src="https://randomuser.me/api/portraits/men/36.jpg" alt="" />
-            <div>
-                <h4 className="name">John Doe</h4>
-                <div className="status">
-                    <p className="status-text"><span className="status-online"></span>{isOnline ? "online" : "offline"}</p>
-                </div>
-            </div>
+function Contact(props) {
+  return (
+    <div className="Contact">
+      <img className="avatar" src={props.avatar} alt={props.name} />
+      <div>
+        <p className="name">{props.name}</p>
+        <div className="status">
+          <div className={props.online ? "status-online" : "status-offline"} />
+          <p className="status-text">{props.online ? "online" : "offline"}</p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  online: PropTypes.bool.isRequired
+};
+
+export default Contact;
