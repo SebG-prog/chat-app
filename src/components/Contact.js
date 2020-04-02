eb5dd3bfb265cab6d10cec80ef8969bbb8e5232d
SebG-prog <sebastien-gruet@orange.fr>
2020-04-02 18:37:15 +0200

    updated Contact.js with state parameters

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,19 +2,30 @@ import React from "react";
 import "./Contact.css";
 import PropTypes from "prop-types";
 
-const Contact = ({name, avatar, online}) => (
+class Contact extends React.Component { 
+  
+  state = {
+    online: this.props.online
+  }
+  
+  render() {
+    //const {avatar,name} = this.props
+    return (
     <div className="Contact">
-      <img className="avatar" src={avatar} alt={name} />
+      <img className="avatar" src={this.props.avatar} alt={this.props.name} />
       <div>
-        <p className="name">{name}</p>
-        <div className="status">
-          <div className={online ? "status-online" : "status-offline"} />
-          <p className="status-text">{online ? "online" : "offline"}</p>
+        <p className="name">{this.props.name}</p>
+        <div className="status" onClick={event => {
+          const status = !this.state.online
+          this.setState({online : status})
+        }}>
+          <div className={this.state.online ? "status-online" : "status-offline"} />
+          <p className="status-text">{this.state.online ? "online" : "offline"}</p>
         </div>
       </div>
     </div>
-  );
-
+  )}
+}
 
 Contact.propTypes = {
   name: PropTypes.string.isRequired,
